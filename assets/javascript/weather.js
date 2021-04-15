3e51eea5dd503e1f6a8ca1dd9605bf5aa5a9602e
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-04-15 21:41:06 +0000

    Add weather info panel from API

---
+++ assets/javascript/weather.js
@@ -0,0 +1,74 @@
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// data
+const weather = {};
+
+weather.temperature = {
+    unit : "celcius"
+}
+
+// constants
+
+const KELVIN = 273;
+
+const key = "5e41c14af1fe1cd2a0a0d31b65fcc73c";
+
+// Check whether browser supports geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// Set position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// Show error for issue with geolocation
+
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`
+}
+
+// Get weather from API
+
+function getWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+console.log(api);
+
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();
+    });
+}
+
+
+
+// Display Weather to UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src = "assets/weather-icons/${weather.iconId}.png">`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
